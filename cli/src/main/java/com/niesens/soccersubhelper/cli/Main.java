106b382ded42d359ae62141cce447625208deea2
cniesen <claus@niesens.com>
2013-09-20 23:49:29 -0500

    Added support of initial player time to cli

--- cli/src/main/java/com/niesens/soccersubhelper/cli/Main.java
+++ cli/src/main/java/com/niesens/soccersubhelper/cli/Main.java
@@ -15,23 +15,21 @@ public class Main {
             return;
         }
 
-        List<Player> players;
         Game game;
+        List<Player> players = getPlayersFromArg(args);
 
         if (args[0].equals("u6")) {
-            if (args.length < 4 || args.length > 7) {
+            if (players.size() < 3 || players.size() > 6) {
                 System.out.println("Invalid number of players.  For U6 between 3 and 6 players are required");
                 return;
             } else {
-                players = getPlayersFromArg(args);
                 game = new Game(AgeGroup.U6, players);
             }
         } else if (args[0].equals("u8")) {
-            if (args.length < 5 || args.length > 9) {
+            if (players.size() < 4 || players.size() > 8) {
                 System.out.println("Invalid number of players.  For U8 between 4 and 8 players are required");
                 return;
             } else {
-                players = getPlayersFromArg(args);
                 game = new Game(AgeGroup.U8, players);
             }
 
@@ -45,21 +43,67 @@ public class Main {
         printPlayerTimes(players);
     }
 
+    /**
+     * Print usage.
+     */
     private static void showHelp() {
-        System.out.println("Usage: <age-group> <player-name> <player-name> ... <player-name>");
+        System.out.println("Usage: <age-group> <player-name> [<play-time>] <player-name> [<play-time>]"
+                + " ... <player-name> [<play-time>]");
         System.out.println();
         System.out.println("  <age-group> \"u6\" or \"u8\" depending on which league the game is in");
         System.out.println("  <player-name> space separated list of player names");
+        System.out.println("  [<play-time>] optional play time in minutes of player");
     }
 
+    /**
+     * Return players.
+     * <p>
+     * Everything after the first argument is considered player information, which may include the players time.
+     *
+     * @param args command line arguments
+     * @return list of Players
+     */
     private static List<Player> getPlayersFromArg(String[] args) {
         List<Player> playerList = new ArrayList<Player>();
 
         for (int i=1; i < args.length; i++) {
-            playerList.add(new Player(args[i]));
+            Player player = new Player(args[i]);
+
+            if (i+1 < args.length) {
+                Integer playTimeMinutes = getPlayTimeMinutesFromArg(args[i+1]);
+                if (playTimeMinutes != null) {
+                    player.setPlayTimeMinutes(playTimeMinutes);
+                    i++; // consume arg
+                }
+            }
+
+            playerList.add(player);
         }
 
         return playerList;
+    }
+
+    /**
+     * Return the play time of the player if specified.
+     * <p>
+     * If the argument is an integer of zero or larger then it's assumed to be the player's play time
+     * and the value gets returned.  For anything else null is being returned to indicate that no play
+     * time for this player was specified.
+     *
+     * @param arg argument string with potential playTimeMinutes value
+     * @return playTimeMinutes or null
+     */
+    private static Integer getPlayTimeMinutesFromArg(String arg) {
+        try {
+            Integer playTimeMinutes = Integer.parseInt(arg);
+            if (playTimeMinutes < 0) {
+                return null;
+            } else {
+                return playTimeMinutes;
+            }
+        } catch (NumberFormatException e) {
+            return null;
+        }
     }
 
     private static void printGameSubstitutionList(Game game) {
